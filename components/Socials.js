9fb6f89b147c5d37d57bfa98ed627f69cfcc7e36
Fernanda Monroe <inandamonroe@gmail.com>
2024-02-06 19:55:31 -0300

    Version 1

---
+++ components/Socials.js
@@ -0,0 +1,33 @@
+// links
+import Link from "next/link";
+
+// icons
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiFacebookLine,
+  RiDribbleLine,
+  RiBehanceLine,
+  RiPinterestLine,
+} from 'react-icons/ri';
+
+const Socials = () => {
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={''} className="hover:text-amber-500 transition-all duration-300">
+        <RiYoutubeLine />
+      </Link>
+      <Link href={''} className="hover:text-amber-500 transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+      <Link href={''} className="hover:text-amber-500 transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={''} className="hover:text-amber-500 transition-all duration-300">
+        <RiPinterestLine />
+      </Link>
+    </div>
+  );
+};
+
+export default Socials;
